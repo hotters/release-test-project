d843762a6e069e63a86f57007f7dda3fb65bae19
Max Gudkov <maksim.gudkov@spark.co.nz>
2022-06-01 22:25:45 +1200

    fix: bugfix

--- commitlint.config.js
+++ commitlint.config.js
@@ -15,7 +15,7 @@ module.exports = {
     "subject-full-stop": [2, "never", "."],
     "type-case": [2, "always", "lower-case"],
     "type-empty": [2, "never"],
-    "type-enum": [2, "always", ["fix", "feat", "breaking", "docs"]],
+    "type-enum": [2, "always", ["fix", "feat", "breaking", "docs", "chore"]],
   },
   prompt: {
     questions: {
